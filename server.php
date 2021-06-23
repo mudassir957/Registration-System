06a438b5f70a8930af59de9d75dde26d3959917b
mudassir957 <62471417+mudassir957@users.noreply.github.com>
2021-06-23 14:23:51 +0500

    Update server.php

--- server.php
+++ server.php
@@ -7,7 +7,7 @@ $email    = "";
 $errors = array(); 
 
 // connect to the database
-$db = mysqli_connect('localhost', 'root', 'test', 'registration');
+$db = mysqli_connect('local', 'root', 'test', 'registration');
 
 // REGISTER USER
 if (isset($_POST['reg_user'])) {
